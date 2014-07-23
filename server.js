73c5d43c2fe89f5cb8ba23a18e9f400261ba23ec
Victor Queiroz <victorcqueirozg@gmail.com>
2014-07-23 00:39:38 -0300

    transfered the bower_components folder contents to /js/vendor of the public folder.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 var express 				= require('express'),
   	bodyParser 			= require('body-parser'),
   	methodOverride 	= require('method-override'),
-  	errorHandler 		= require('error-handler'),
+  	errorhandler 		= require('errorhandler'),
   	morgan 					= require('morgan'),
   	routes 					= require('./routes'),
   	models 					= require('./models'),
@@ -20,7 +20,7 @@ var app = module.exports = express();
  * Configuration
  */
 
-// all environments
+// All environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,17 +32,21 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Transfer the 'bower_components' folder contents to '/js/vendor' of the 'public' folder.
+ */
+app.use('/js/vendor', express.static(path.join(__dirname, 'bower_components')));
+
 var env = process.env.NODE_ENV || 'development';
 
-// // development only
-// if (env === 'development') {
-//   app.use(express.errorHandler());
-// }
+// Development only
+if (env === 'development') {
+  app.use(errorhandler());
+}
 
-// // production only
-// if (env === 'production') {
-//   // TODO
-// }
+// Production only
+if (env === 'production') {
+}
 
 /**
  * Routes
@@ -56,6 +60,8 @@ app.get('/api/users/:id', users.get);
 app.post('/api/users', users.store);
 app.put('/api/users/:id', users.update);
 app.delete('/api/users/:id', users.destroy);
+
+app.get('*', routes.index);
 
 /**
  * Start the server
